6c2381546b5e85877a969473a2969f600cd253cc
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-08-02 12:43:56 +0300

    Добавьте файлы проекта.

---
+++ BankLibrary/IAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary {
+    interface IAccount {
+        // Положить деньги на счет
+        void Put(decimal sum);
+        // Взять деньги со счета
+        decimal Withdraw(decimal sum);
+    }
+}
---
+++ ConsoleClassLibrary/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ConsoleClassLibrary {
+    // Консольное приложение имитации работы банка
+    class Program {
+        static void Main(string[] args) {
+            Console.ReadKey();
+        }
+    }
+}
